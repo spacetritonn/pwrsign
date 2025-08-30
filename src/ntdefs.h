d8112d01c904e3b6443fc59d540e5ab3bf31f7c2
Triton <nikoharianto58@gmail.com>
2025-08-30 19:13:02 +0700

    Update ntdefs.h

--- src/ntdefs.h
+++ src/ntdefs.h
@@ -1,6 +1,1 @@
-#pragma once
-#include "pwrsign.h"
 
-typedef NTSTATUS(NTAPI* fRtlAdjustPrivilege)(_In_ DWORD Privilege, _In_ BOOLEAN Enable, _In_ BOOLEAN CurrentThread, _Out_ PBOOLEAN Enabled);
-typedef NTSTATUS(NTAPI* fNtPowerInformation)(_In_ POWER_INFORMATION_LEVEL InformationLevel, _In_reads_bytes_opt_(InputBufferLength) PVOID InputBuffer, _In_ DWORD InputBufferLength, _Out_writes_bytes_opt_(OutputBufferLength) PVOID OutputBuffer, _In_ DWORD OutputBufferLenght);
-typedef NTSTATUS(NTAPI* fNtInitiatePowerAction)(_In_ POWER_ACTION SystemAction, _In_ SYSTEM_POWER_STATE LightestSystemState, _In_ DWORD Flags, _In_ BOOLEAN Asynchronous);
